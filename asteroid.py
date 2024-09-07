3730500fd50a880a1235a063e73abc4e01af89d2
garvamel <garvamel@gmail.com>
2024-09-06 20:40:17 -0400

    Implemented collisions

---
+++ asteroid.py
@@ -0,0 +1,19 @@
+import pygame
+from circleshape import *
+from constants import *
+
+class Asteroid(CircleShape):
+
+    containers = ()
+
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.velocity = 0
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255,255,255), self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+
+        
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame
+import sys
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
@@ -9,13 +13,38 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatables, drawables)
+    Asteroid.containers = (asteroids, updatables, drawables)
+    AsteroidField.containers =(updatables)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfiled = AsteroidField()
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        
+        for updt in updatables:
+            updt.update(dt)
+        for drw in drawables:
+            drw.draw(screen)
+        for astrd in asteroids:
+            if astrd.check_collision(player):
+                print("Game over!")
+                sys.exit()
+        
         pygame.display.flip()
-
-
+        dt = game_clock.tick(60)/1000
+        
 if __name__ == "__main__":
     main()
